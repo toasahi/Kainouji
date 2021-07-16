0abf988d1dbcb05021af8f55f33707ab9c974be0
toasahi <asa1313to@gmail.com>
2021-07-16 23:22:53 +0900

    Add option PDO

--- hard/DB/Database.php
+++ hard/DB/Database.php
@@ -3,9 +3,10 @@ require_once ("config/db_config.php");
 
 class Database {
     public $pdo;
+    private $option = [PDO::ATTR_EMULATE_PREPARES=>false];
 
     public function __construct(){
-        $this->pdo = new PDO(DB_DNS,DB_USER,DB_PASS);
+        $this->pdo = new PDO(DB_DNS,DB_USER,DB_PASS,$this->option);
         if($this->pdo === null){
             die("Connection Error:");
         }
