31592110158c22bff706682c99279b713d6de526
toasahi <asa1313to@gmail.com>
2021-09-03 08:36:13 +0900

    Add BME280

--- hard/DataRecive.php
+++ hard/DataRecive.php
@@ -1,7 +1,13 @@
 <?php
 require_once 'DB/Data.php';
 $moisture = $_POST['moisture'];
+$temerature = $_POST['temerature'];
+$humidity = $_POST['humidity'];
+$air_pressure = $_POST['air_pressure'];
+echo $air_pressure;
+echo $humidity;
+echo $temerature;
 $dataHandle = new Data();
-if(isset($moisture)){
-    $dataHandle->insertSoilMoisture($moisture);
-}
+// if(isset($moisture)){
+//     $dataHandle->insertSoilMoisture($moisture);
+// }
