67b00bea5fba8ed902d387d3e29506203620d27e
toasahi <asa1313to@gmail.com>
2021-09-05 13:00:44 +0900

    Change Get method

--- hard/DataRecive.php
+++ hard/DataRecive.php
@@ -1,11 +1,9 @@
 <?php
 require_once 'DB/Data.php';
-// $moisture = $_POST['moisture'];
 $moisture = $_GET['moisture'];
-// $temerature = $_GET['temerature'];
-// $humidity = $_GET['humidity'];
-// $air_pressure = $_GET['air_pressure'];
-// var_dump($_POST);
+$temerature = $_GET['temerature'];
+$humidity = $_GET['humidity'];
+$air_pressure = $_GET['air_pressure'];
 $dataHandle = new Data();
 if(isset($moisture)){
     $dataHandle->insertSoilMoisture($moisture);
