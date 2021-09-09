39c62ba0b9ec51a303dddec7323507bb3f3a354f
toasahi <asa1313to@gmail.com>
2021-09-09 17:39:29 +0900

    change type

--- web/src/types/api/field.ts
+++ web/src/types/api/field.ts
@@ -1,11 +1,21 @@
+// export type Field = {
+//   id: number;
+//   fieldName: string;
+//   vegetable_id: number;
+//   threshold_id: number;
+//   setting_date: string;
+//   place: string;
+//   imageUrl: File;
+//   created_at: string;
+//   updated_at: string;
+// };
+
 export type Field = {
   id: number;
-  fieldName: string;
+  field_name: string;
   vegetable_id: number;
-  threshold_id: number;
   setting_date: string;
-  place: string;
-  imageUrl: File;
+  image_path: string;
   created_at: string;
   updated_at: string;
 };
