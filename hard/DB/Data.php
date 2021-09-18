f5545dc5fca5da1f2425d2e919e07190b30e8366
toasahi <asa1313to@gmail.com>
2021-09-19 00:19:41 +0900

    Delete functions & Add selectThresholds function id

--- hard/DB/Data.php
+++ hard/DB/Data.php
@@ -6,103 +6,12 @@ class Data extends Database
 {
 
     /**
-     * 土壌の水分量を追加
-     * @param int $moisture
-     * @return boolean $result
-     */
-
-    public function insertSoilMoisture($moisture, $input_parameters = NULL)
-    {
-        $sql = "INSERT INTO soil_moistures (moisture,field_id) VALUES ('{$moisture}',1)";
-        $stmt = $this->pdo->prepare($sql);
-        $result = $stmt->execute($input_parameters);
-        return $result;
-    }
-
-    /**
-     * 土壌の水分量を取得
-     * @return array|boolean $result
-     */
-
-    public function selectSoilMoisture($input_parameters = NULL)
-    {
-        $sql = "SELECT * FROM soil_moistures";
-        $stmt = $this->pdo->prepare($sql);
-        $flag = $stmt->execute($input_parameters);
-        if (!$flag) {
-            return false;
-        }
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        return $result;
-    }
-
-    /**
-     * 空気中の湿度を追加
-     * @param int $humidity
-     * @return boolean $result
-     */
-    public function insertHumidity($humidity, $input_parameters = NULL)
-    {
-        $sql = "INSERT INTO humidities (humidity) VALUES ('{$humidity}')";
-        $stmt = $this->pdo->prepare($sql);
-        $result = $stmt->execute($input_parameters);
-        return $result;
-    }
-
-    /**
-     * 空気中の湿度を取得
-     * @return array|boolean $result
-     */
-
-    public function selectHumidity($input_parameters = NULL)
-    {
-        $sql = "SELECT * FROM humidities";
-        $stmt = $this->pdo->prepare($sql);
-        $flag = $stmt->execute($input_parameters);
-        if (!$flag) {
-            return false;
-        }
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        return $result;
-    }
-
-    /**
-     * 空気中の温度を追加
-     * @param int $temperature
-     * @return boolean $result
-     */
-    public function insertTemperature($temperature, $input_parameters = NULL)
-    {
-        $sql = "INSERT INTO temperatures (temperature) VALUES ('{$temperature}')";
-        $stmt = $this->pdo->prepare($sql);
-        $result = $stmt->execute($input_parameters);
-        return $result;
-    }
-
-    /**
-     * 空気中の温度を取得
-     * @return array|boolean $result
-     */
-
-    public function selectTemperature($input_parameters = NULL)
-    {
-        $sql = "SELECT * FROM temperatures";
-        $stmt = $this->pdo->prepare($sql);
-        $flag = $stmt->execute($input_parameters);
-        if (!$flag) {
-            return false;
-        }
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        return $result;
-    }
-
-
-    /**
      * データの閾値を取得する
+     * @param string $id
      * @return array $result
      */
-    public function selectThresholdData($input_parameters = NULL){
-        $sql = "SELECT moisture,temperature_high,temperature_low,air_pressure FROM thresholds";
+    public function selectThresholdData($id,$input_parameters = NULL){
+        $sql = "SELECT moisture,temperature_high,temperature_low,air_pressure FROM thresholds WHERE id = '$id'";
         $stmt = $this->pdo->prepare($sql);
         $flag = $stmt->execute($input_parameters);
         if(!$flag){
--- hard/DataRecive.php
+++ hard/DataRecive.php
@@ -6,8 +6,7 @@ $humidity = $_GET['humidity'];
 $air_pressure = $_GET['air_pressure'];
 $dataHandle = new Data();
 if(isset($moisture)){
-    $dataHandle->insertSoilMoisture($moisture);
-    $dataHandle->insertFieldData($moisture,$temerature,$humidity,$air_pressure,1);
+    $dataHandle->insertFieldData($moisture,$temerature,$humidity,$air_pressure,6);
 }else{
     print("error");
 }
