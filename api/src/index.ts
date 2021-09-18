b3d51e7efc63b6a6e9f2c1e90a186318d9ae19a7
toasahi <asa1313to@gmail.com>
2021-09-19 00:14:32 +0900

    Add cors modules

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
+import path from 'path'
+import cors from 'cors'
 import { router } from './v1/index';
 
 const app: express.Express = express();
@@ -11,6 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 
 //ルーティング
 app.use('/v1', router);
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(cors())
 
 //サーバー起動
 app.listen(4000, () => console.log('Express Server Now Running On localhost:4000'));
