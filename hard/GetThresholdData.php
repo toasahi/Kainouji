9d05008a34445657fa6b5a98135f6968ccd9e4c8
toasahi <asa1313to@gmail.com>
2021-11-07 19:10:57 +0900

    Add Esp32 id

--- hard/GetThresholdData.php
+++ hard/GetThresholdData.php
@@ -3,6 +3,6 @@ require_once 'DB/Data.php';
 $dataHandle = new Data();
 
 //モータの閾値を取得
-$threshodData = $dataHandle->selectThresholdData(6);
+$threshodData = $dataHandle->selectThresholdData(7);
 
 echo json_encode($threshodData[0]);
