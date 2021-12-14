ac5f1580f50eb53e009038fa1fca0c9534a6db07
toasahi <asa1313to@gmail.com>
2021-12-14 14:19:34 +0900

    Firebaseの設定を追加

--- web/src/constant/BaseCss.ts
+++ web/src/constant/BaseCss.ts
@@ -590,72 +590,72 @@ const SSecondLogin = styled.div`
 `;
 
 const SSecondSignUp = styled.div`
-    main {
-      margin: 0 auto;
-      border: 1px solid #e8e6e6;
-      margin-top: 50px;
-      width: 65%;
-      background-color: #fefefe;
-      padding: 30px 20px;
-      border-radius: 10px;
+  main {
+    margin: 0 auto;
+    border: 1px solid #e8e6e6;
+    margin-top: 50px;
+    width: 65%;
+    background-color: #fefefe;
+    padding: 30px 20px;
+    border-radius: 10px;
 
-      .user-info-container {
+    .user-info-container {
+      margin-top: 30px;
+    }
+
+    label {
+      line-height: 3;
+      opacity: 75%;
+    }
+
+    .button-container {
+      text-align: center;
+      button {
+        margin: 0 auto;
         margin-top: 30px;
-      }
 
-      label {
-        line-height: 3;
-        opacity: 75%;
-      }
-
-      .button-container {
-        text-align: center;
-        button {
-          margin: 0 auto;
-          margin-top: 30px;
-
-          &:after {
-            content: '';
-            width: 0px;
-            height: 0px;
-            border: 0;
-          }
-        }
-      }
-
-      span {
-        color: #ed4956;
-        font-size: ${Font.textSm};
-        line-height: 2;
-      }
-
-      .account-nav {
-        display: flex;
-        margin-top: 20px;
-        padding: 10px 20px;
-        justify-content: center;
-
-        .account-nav-item {
-          label{
-            font-size:${Font.textSm};
-          }
-          a {
-            margin-left: 10px;
-            opacity: 50%;
-            &:hover {
-              opacity: 100%;
-            }
-          }
+        &:after {
+          content: '';
+          width: 0px;
+          height: 0px;
+          border: 0;
         }
       }
     }
 
-  @media(min-width:${Responsive.sm}){
-    main{
-      width:30%;
+    span {
+      color: #ed4956;
+      font-size: ${Font.textSm};
+      line-height: 2;
+    }
 
-      label{
-        font-size:${Font.textBase};
+    .account-nav {
+      display: flex;
+      margin-top: 20px;
+      padding: 10px 20px;
+      justify-content: center;
+
+      .account-nav-item {
+        label {
+          font-size: ${Font.textSm};
+        }
+        a {
+          margin-left: 10px;
+          opacity: 50%;
+          &:hover {
+            opacity: 100%;
+          }
+        }
+      }
+    }
+  }
+
+  @media (min-width: ${Responsive.sm}) {
+    main {
+      width: 30%;
+
+      label {
+        font-size: ${Font.textBase};
       }
     }
   }
@@ -683,7 +683,7 @@ const SLookField = styled(BaseContainer)`
         bottom: 0;
         left: 50%;
         transform: translate(-50%, 0);
-        width:30%;
+        width: 30%;
         opacity: 25%;
         height: 15px; /* 線幅 */
       }
@@ -947,7 +947,7 @@ const SGraph = styled.div`
   }
 
   @media (min-width: ${Responsive.sm}) {
-    main{
+    main {
       h1 {
         font-size: ${Font.text4xl};
         padding: 5px;
@@ -955,7 +955,7 @@ const SGraph = styled.div`
         margin-top: 75px;
         margin-bottom: 30px;
       }
-    
+
       .graph {
         display: flex;
         justify-content: space-between;
